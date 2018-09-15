bb8206c8145efa6ee5205ebefd67d74fc8a0352f
Daniel <d_sanchez@ar3d.net>
2018-09-15 11:32:23 -0500

    Added pods.

--- PegasusEvaluation/ViewController.swift
+++ PegasusEvaluation/ViewController.swift
@@ -7,18 +7,77 @@
 //
 
 import UIKit
-
+import Alamofire
+import AEXML
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getJson2()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getJson() {
+        guard let url = URL(string: "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL")
+            else {
+                return
+        }
+        let headers: HTTPHeaders = [
+            "Content-Type": "text/xml"
+        ]
+        
+        Alamofire.request(url,
+                          method: .post,
+                          parameters: ["include_docs": "true"])
+            .validate()
+            .responseData(completionHandler: { (response) in
+                print(response)
+            })
+//            .responseJSON { response in
+//                guard response.result.isSuccess else {
+//                    print("Error")
+//                    return
+//                }
+//                guard let value = response.result.value as? [String: Any],
+//                    let rows = value["rows"] as? [[String: Any]] else {
+//                        print("Malformed data received from fetchAllRooms service")
+//                        return
+//                }
+//
+//        }
+        
+        
+    }
+    func getJson2() {
+        
+        let soapRequest = AEXMLDocument()
+        if let url = URL(string: "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL") {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = HTTPMethod.get.rawValue
+            
+            var headers: HTTPHeaders
+            if let existingHeaders = urlRequest.allHTTPHeaderFields {
+                headers = existingHeaders
+            } else {
+                headers = HTTPHeaders()
+            }
+            headers["Content-Type"] = "text/xml"
+            urlRequest.allHTTPHeaderFields = headers
+            
+            Alamofire.request(urlRequest)
+                .responseData(completionHandler: { (response) in
+                    debugPrint(response)
+                })
+            
+            debugPrint(request)
+        }
+    }
+
 
 
 }
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'PegasusEvaluation' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  pod 'Alamofire'
+  pod 'AEXML'
+  #pod 'AFNetworking', '~> 3.0'
+  # Pods for PegasusEvaluation
+  target 'PegasusEvaluationTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PegasusEvaluationUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
