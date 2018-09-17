1103e6139cca5751dd02785ce5bde809f7baedad
Daniel <d_sanchez@ar3d.net>
2018-09-17 17:33:42 -0500

    WebService is now being called with Alamofire using AEXML

--- PegasusEvaluation/ConverterViewModel.swift
+++ PegasusEvaluation/ConverterViewModel.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AEXML
+import Alamofire
 
 class ConverterViewModel: NSObject {
     
@@ -18,21 +20,67 @@ class ConverterViewModel: NSObject {
     
     func convert(celsius:String) {
         if celsius.isNumber() {
-            let converter = TempConvert()
-            let result = converter.CelsiusToFahrenheit(Celsius: celsius)
-            delegate?.convertionFinished(farenheit: result)
-//            if delegate != nil {
-//
-//            }
-            db.insertConversion(celsius: celsius, fahrenheit: result)
-            listConversions()
+            //the lines above use no framework, but a page that generates code for consuming soap services.
+            //I'm leaving them just for reference.
+            //let converter = TempConvert()
+            //let result = converter.CelsiusToFahrenheit(Celsius: celsius)
+            convertCelsiusToFahrenheit(celsius: celsius, completion: { (response) in
+                print("finished \(response)")
+                if self.delegate != nil {
+                    self.delegate!.convertionFinished(farenheit: response)
+                }
+                self.db.insertConversion(celsius: celsius, fahrenheit: response)
+                self.listConversions()
+            })
         } else {
-            delegate?.convertionEndedWithError(message: "Por favor introduce un número")
+            if delegate != nil{
+                delegate!.convertionEndedWithError(message: "Por favor introduce un número")
+            }
         }
     }
+    
     func listConversions() {
         for conversionLog in db.readConversions() {
             print(conversionLog)
+        }
+    }
+    
+    //This should be in a different file
+    func convertCelsiusToFahrenheit(celsius: String, completion: @escaping (_ result: String) -> Void) -> Void {
+        let soapRequest = AEXMLDocument()
+        let attributes = [
+            "xmlns:xsi" : "http://www.w3.org/2001/XMLSchema-instance",
+            "xmlns:xsd" : "http://www.w3.org/2001/XMLSchema",
+            "xmlns:soap":"http://schemas.xmlsoap.org/soap/envelope/"
+        ]
+        let envelope = soapRequest.addChild(name: "soap:Envelope", attributes: attributes)
+        let body = envelope.addChild(name: "soap:Body")
+        let celsiusToF = body.addChild(name: "CelsiusToFahrenheit",attributes:["xmlns":"https://www.w3schools.com/xml/"])
+        celsiusToF.addChild(name:"Celsius",value:celsius)
+        
+        let soapLenth = String(soapRequest.xml.characters.count)
+        
+        let url = URL(string:"https://www.w3schools.com/xml/tempconvert.asmx?wsdl")
+        var xmlRequest = URLRequest(url: url!)
+        xmlRequest.httpBody = soapRequest.xml.data(using: String.Encoding.utf8)
+        xmlRequest.httpMethod = "POST"
+        xmlRequest.addValue("text/xml;charset =utf-8", forHTTPHeaderField: "Content-Type")
+        xmlRequest.addValue(soapLenth, forHTTPHeaderField: "Content-Length")
+        
+        Alamofire.request(xmlRequest).responseString { (response) in
+            if let xmlString = response.result.value {
+                let xml = SWXMLHash.parse(xmlString)
+                let body =  xml["soap:Envelope"]["soap:Body"]
+                if let resultElement = body["CelsiusToFahrenheitResponse"]["CelsiusToFahrenheitResult"].element {
+                    let finalResult = resultElement.text
+                    completion(finalResult)
+                }
+            }else{
+                print("error fetching XML")
+                
+            }
+            
+            
         }
     }
 }
